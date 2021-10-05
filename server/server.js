18ea4504e55dc71d0d989fa0b737e3194c4e74be
Mari Franco <marifranco@Maris-Air-7.home>
2021-10-04 21:25:33 -0700

    testing-data

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+//const { animals } = require('./data/animals');
 const app = express();
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
+
+
+
+
