f4bfb1c525e700b394ef6d22bf107a0a4744e90f
Mari Franco <marifranco@Maris-Air-7.home>
2021-10-04 22:33:11 -0700

    server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,58 @@
-const express = require('express');
-//const { animals } = require('./data/animals');
-const app = express();
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const notes = require("../server.json");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME, SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
 
+const app = express();
+var PORT = process.env.PORT || 3001;
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// STATIC MIDDLEWARE
+app.use(express.static("./public"));
+
+
+
+//GET API db.json
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../server.json"))
+});
+
+// Post function to add new notes to server.json
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("../server.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
+
+//used for deleting notes
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("../server.json"));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+//HTML calls
+//calls home page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+//call for notes.html
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+//Start listen
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
 
 
 
